337a946f6e9c957f277bc5bfb592d8949283de05
root <root@7f7c322f5e0f>
2022-04-19 06:29:26 +0000

    Added csv. file in the repository.

--- AI Badge Data/best_pipeline.py
+++ AI Badge Data/best_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('AI_Badge_03/AI Badge Data/prepared_data.csv')
@@ -10,7 +10,7 @@ training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
 # Average CV score on the training set was: 1.0
-exported_pipeline = DecisionTreeClassifier(criterion="entropy", max_depth=8, min_samples_leaf=10, min_samples_split=15)
+exported_pipeline = RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.5, min_samples_leaf=1, min_samples_split=13, n_estimators=100)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
