5925af78a28e815768773b293cc8b2ea4675ce9d
root <root@acc035970232>
2022-05-16 10:42:30 +0000

    Added csv. file in the repository.

--- AI Badge Data/best_pipeline.py
+++ AI Badge Data/best_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('AI_Badge_03/AI Badge Data/prepared_data.csv')
@@ -10,7 +10,7 @@ training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
 # Average CV score on the training set was: 1.0
-exported_pipeline = GradientBoostingClassifier(learning_rate=0.1, max_depth=1, max_features=0.6000000000000001, min_samples_leaf=12, min_samples_split=7, n_estimators=100, subsample=0.6500000000000001)
+exported_pipeline = DecisionTreeClassifier(criterion="entropy", max_depth=10, min_samples_leaf=6, min_samples_split=3)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
