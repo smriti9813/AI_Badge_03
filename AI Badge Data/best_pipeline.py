4c7a7ae6672a87586b84de8d7a6c8fdac11cb7b9
root <root@9fdb1bee447e>
2022-06-01 09:45:06 +0000

    Added csv. file in the repository.

--- AI Badge Data/best_pipeline.py
+++ AI Badge Data/best_pipeline.py
@@ -10,7 +10,7 @@ training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
 # Average CV score on the training set was: 1.0
-exported_pipeline = GradientBoostingClassifier(learning_rate=0.5, max_depth=3, max_features=0.5, min_samples_leaf=19, min_samples_split=5, n_estimators=100, subsample=0.35000000000000003)
+exported_pipeline = GradientBoostingClassifier(learning_rate=0.01, max_depth=3, max_features=0.9000000000000001, min_samples_leaf=17, min_samples_split=7, n_estimators=100, subsample=0.55)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
